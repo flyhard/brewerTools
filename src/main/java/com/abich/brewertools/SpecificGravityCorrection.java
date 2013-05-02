53a6099f39b52791b8df20ee6900f8a0c5bdc70e
Per Abich <per.abich@gmail.com>
2013-05-02 22:57:53 +0200

    added

---
+++ src/main/java/com/abich/brewertools/SpecificGravityCorrection.java
@@ -0,0 +1,12 @@
+package com.abich.brewertools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SpecificGravityCorrection extends Activity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_specific_gravity_correction);
+}
+}
