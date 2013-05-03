c2693c696d192e75552029cff43ad414046dfb49
Per Abich <per.abich@visma.com>
2013-05-03 12:15:25 +0200

    Added blank test

---
+++ src/test/java/com/abich/brewertools/SpecificGravityCorrectionTest.java
@@ -0,0 +1,14 @@
+package com.abich.brewertools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SpecificGravityCorrectionTest {
+
+	@Test
+	public void testOnCreate() {
+		fail("Not yet implemented");
+	}
+
+}
